bea24e68ee14768bc7e3dd3160e95a3483c7549a
Souza <38145379+misael28@users.noreply.github.com>
2020-10-21 19:47:33 -0300

    requisicoes API propria e CSS

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 const arrow_left = require("./arrow_left.svg");
 const arrow_right = require("./arrow_right.svg");
 const arrow_sort = require("./arrow_sort.svg");
-//let dadosClassificao = [];
 let brasaoCasa = [];
-let brasaoVis = []
+let brasaoVis = [];
+let brasao = [];
+
 const escudos = [
   {
     nomeEscudo: "Flamengo",
@@ -36,8 +37,8 @@ const escudos = [
     url: "https://www.escudosfc.com.br/images/interrs.png",
   },
   {
-    nomeEscudo: "Corinthias",
-    url: "https://www.escudosfc.com.br/images/corinthias.png",
+    nomeEscudo: "Corinthians",
+    url: "https://www.escudosfc.com.br/images/corinthians.png",
   },
   {
     nomeEscudo: "Fortaleza",
@@ -89,33 +90,91 @@ const escudos = [
   },
 ];
 
+const colunas = [
+  "nome",
+  "pontos",
+  "jogos",
+  "vitorias",
+  "derrotas",
+  "empates",
+  "golsFeitos",
+  "golsSofridos",
+];
+
+const legenda = {
+  nome: "Time",
+  pontos: "Pontos",
+  jogos: "J",
+  vitorias: "V",
+  derrotas: "D",
+  empates: "E",
+  golsFeitos: "GF",
+  golsSofridos: "GS",
+};
+
+function fazerRequisicaoComBody(url, metodo, conteudo, token) {
+  return fetch(url, {
+    method: metodo,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token && `Bearer ${token}`,
+    },
+    body: JSON.stringify(conteudo),
+  });
+}
+
 function App() {
   const [tabela, setTabela] = React.useState([]);
   const [rodadas, setRodadas] = React.useState([]);
   const [avancar, setAvancar] = React.useState(1);
+  const [colunaOrdenada, setColunaOrdenada] = React.useState("pontos");
+  const [ordem, setOrdem] = React.useState("ascendente");
+
   /**
    * Chamando a API
    */
   //tabela de classificação
   React.useEffect(() => {
-    {
-      /*	fetch("localhost:2020/classificacao")*/
-    }
-    fetch("https://desafio-3-back-cubos-academy.herokuapp.com/classificacao")
+   
+    fetch("http://localhost:2020/classificacao")
       .then((res) => res.json())
       .then((dados) => {
         setTabela(dados.dados);
       });
+    {
+      /*fazerRequisicaoComBody(
+      "https://localhost:2020/classificacao",
+      "GET",
+      conteudo,
+      token
+	);
+	*/
+    }
   }, []);
 
   React.useEffect(() => {
-    fetch(`https://desafio-3-back-cubos-academy.herokuapp.com/jogos/${avancar}`)
+    fetch(`http://localhost:2020/jogos/${avancar}`)
       .then((res) => res.json())
       .then((dados) => {
         setRodadas(dados.dados);
-        console.log(dados.dados);
       });
   }, [avancar]);
+
+  const dadosAscendentes = tabela.sort((t1, t2) => {
+    if (
+      typeof t1[colunaOrdenada] === "number" &&
+      typeof t2[colunaOrdenada] === "number"
+    ) {
+      return (
+        parseInt(t2[colunaOrdenada], 10) - parseInt(t1[colunaOrdenada], 10)
+      );
+    } else {
+      return t1[colunaOrdenada].localeCompare(t2[colunaOrdenada]);
+    }
+  });
+
+  const dadosOrdenados =
+    ordem === "ascendente" ? dadosAscendentes : dadosAscendentes.reverse();
 
   return (
     <div className="App">
@@ -162,83 +221,96 @@ function App() {
           </div>
           <div className="rodadas-conteudo">
             <ul className="jogos">
-              {rodadas.map((partida) => (
-				  escudos.forEach(element => {
-					  if(element.nomeEscudo===partida.time_casa){
-						  brasaoCasa=element.url
-					  }
-					  if(element.nomeEscudo===partida.time_visitante){
-						  brasaoVis=element.url
-					  }
-				  }),
-                <li>
-					
-                  <div><img src={brasaoCasa} alt={partida.time_casa}/></div>
-                  <div>{partida.gols_casa} x {partida.gols_visitante}</div>
-                  <div><img src={brasaoVis} alt={partida.time_visitante}/></div>
-                </li>
-              ))}
+              {rodadas.map(
+                (partida) => (
+                  escudos.forEach((element) => {
+                    if (element.nomeEscudo === partida.time_casa) {
+                      brasaoCasa = element.url;
+                    }
+                    if (element.nomeEscudo === partida.time_visitante) {
+                      brasaoVis = element.url;
+                    }
+                  }),
+                  (
+                    <li>
+                      <div>
+                        <img
+                          src={brasaoCasa}
+                          alt={partida.time_casa}
+                          title={partida.time_casa}
+                        />
+                      </div>
+                      <div>
+                        {partida.gols_casa} x {partida.gols_visitante}
+                      </div>
+                      <div>
+                        <img
+                          src={brasaoVis}
+                          alt={partida.time_visitante}
+                          title={partida.time_visitante}
+                        />
+                      </div>
+                    </li>
+                  )
+                )
+              )}
             </ul>
           </div>
         </div>
-				{/*   brasao = escudos.filter(
+        {/*   brasao = escudos.filter(
                       (item) => item.nomeEscudo === partida.time_casa
                 );*/}
 
         <div className="classifica">
           <div className="classifica-header">
             <label>Posição</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>Time</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>PTS</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>E</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>V</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>D</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>GF</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>GS</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
-            <label>SG</label>
-            <button>
-              <img src={arrow_sort} alt="sort" />
-            </button>
+            {colunas.map((coluna) => (
+              <label>
+                {legenda[coluna]}{" "}
+                <button
+                  onClick={() => {
+                    if (colunaOrdenada === coluna) {
+                      setOrdem((ordem) =>
+                        ordem === "descendente" ? "ascendente" : "descendente"
+                      );
+                    } else {
+                      setColunaOrdenada(coluna);
+                      setOrdem("descendente");
+                    }
+                  }}
+                >
+                  {colunaOrdenada !== coluna || ordem === "descendente"
+                    ? "⬇"
+                    : "⬆"}
+                </button>
+              </label>
+            ))}
           </div>
           <div className="classifica-lista">
             <ul className="classifca-lista-conteudo">
-              {tabela.map((time, i) => (
-                <li>
-                  <div>{i + 1}</div>
-                  <div>{time.nome}</div>
-                  <div>{time.pontos}</div>
-                  <div>{time.jogos}</div>
-                  <div>{time.vitorias}</div>
-                  <div>{time.derrotas}</div>
-                  <div>{time.empates}</div>
-                  <div>{time.golsFeitos}</div>
-                  <div>{time.golsSofridos}</div>
-                </li>
-              ))}
+              {/*tabela.map */}
+              {dadosOrdenados.map(
+                (time, i) => (
+                  escudos.forEach((element) => {
+                    if (element.nomeEscudo === time.nome) {
+                      brasao = element.url;
+                    }
+                  }),
+                  (
+                    <li>
+                      <div className="indice-posicao">{i + 1}</div>
+                      <img src={brasao} alt={time.nome} title={time.nome}></img>
+                      <div className="indice-posicao">{time.pontos}</div>
+                      <div className="indice-posicao">{time.jogos}</div>
+                      <div className="indice-posicao">{time.vitorias}</div>
+                      <div className="indice-posicao">{time.derrotas}</div>
+                      <div className="indice-posicao">{time.empates}</div>
+                      <div className="indice-posicao">{time.golsFeitos}</div>
+                      <div className="indice-posicao">{time.golsSofridos}</div>
+                    </li>
+                  )
+                )
+              )}
             </ul>
             {/*
 			<div className="tables">
